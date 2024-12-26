3e2decb3c33a068d5c620301a365225a203e71ee
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-26 18:47:13 +0000

    Initial commit + Ticket form, see TicketForm.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './styles.css'
+import TicketForm from "./components/TicketForm.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+          <TicketForm />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TicketForm.jsx
@@ -0,0 +1,69 @@
+import React, {useState} from "react";
+import '../styles.css'
+
+const TicketForm = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority, setPriority] = useState('1');
+
+    const priorityLabels = {
+        1: 'Low',
+        2: 'Medium',
+        3: 'High'
+    }
+
+    const clearForm = () => {
+        setTitle('');
+        setDescription('');
+        setPriority('1');
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault(); //blocks from reloading the form
+        const ticket = {
+            id: new Date().toISOString(),
+            title,
+            description,
+            priority
+        };
+        clearForm(); //Form should reset after a submit
+    }
+
+    return (
+        <form className="ticket-form" onSubmit={handleSubmit}>
+            <div>
+                <label>Title</label>
+                <input type="text"
+                       value={title}
+                       className="form-input"
+                       onChange={evt => setTitle(evt.target.value)}></input>
+            </div>
+            <div>
+                <label>Description</label>
+                <textarea
+                    value={description}
+                    className="form-input"
+                    onChange={evt => setDescription(evt.target.value)}></textarea>
+            </div>
+            <fieldset className="priority-fieldset">
+                <legend>Priority</legend>
+                {
+                    //This is a way to get entries from a map in JS object
+                    Object.entries(priorityLabels).map(([value, label]) => (
+                        <label key={value} className="priority-label">
+                            <input type="radio"
+                                   value={value}
+                                   checked={priority === value}
+                                   className="priority-input"
+                                   onChange={evt => setPriority(evt.target.value)} />
+                            {label}
+                        </label>
+                    ))
+                }
+            </fieldset>
+            <button className="button" type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default TicketForm;
